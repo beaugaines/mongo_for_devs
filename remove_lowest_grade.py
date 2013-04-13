745d25224438230bdab8f5be6b50de284badfad0
Charlie Gaines <beaugaines@gmail.com>
2013-04-12 22:02:21 -0400

    remove_lowest_grade working

--- remove_lowest_grade.py
+++ remove_lowest_grade.py
@@ -14,14 +14,17 @@ grades = db.grades
 
 def remove_lowest_homework():
     query = { 'type' : 'homework' }
-    selector = { 'student_id' : 1 , 'score' : 1, '_id' : 0 }
 
     try:
         homeworks = grades.find(query).sort([['student_id', pymongo.ASCENDING], ['score', pymongo.ASCENDING]])
     except:
         print 'Query failed: ', sys.exc_info()[0]
 
-    for rec in homeworks:
-        print rec
 
-remove_lowest_homework()
+    index = 0
+    for rec in homeworks:
+        if index == rec['student_id']:
+            grades.remove(rec)
+            index +=1
+
+
