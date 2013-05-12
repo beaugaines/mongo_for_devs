08c154700098538e6084a55c17fcee9e2b67abd5
Charlie Gaines <beaugaines@gmail.com>
2013-05-12 16:39:48 -0400

    finished wk6 probs

--- wk6_scripts/create_replica_set.sh
+++ wk6_scripts/create_replica_set.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p /home/sircharles/rs1 /home/sircharles/rs2 /home/sircharles/rs3
-mongod --replSet m101 --logpath "1.log" --dbpath /home/sircharles/data/rs1 --port 27018 --smallfiles --fork
-mongod --replSet m101 --logpath "2.log" --dbpath /home/sircharles/data/rs2 --port 27019 --smallfiles --fork
-mongod --replSet m101 --logpath "3.log" --dbpath /home/sircharles/data/rs3 --port 27020 --smallfiles --fork
+mkdir -p /home/sircharles/data/rs1 /home/sircharles/data/rs2 /home/sircharles/data/rs3
+mongod --replSet m101 --logpath "1.log" --dbpath /home/sircharles/data/rs1 --port 27017 --smallfiles --fork
+mongod --replSet m101 --logpath "2.log" --dbpath /home/sircharles/data/rs2 --port 27018 --smallfiles --fork
+mongod --replSet m101 --logpath "3.log" --dbpath /home/sircharles/data/rs3 --port 27019 --smallfiles --fork
--- wk6_scripts/init_replica.js
+++ wk6_scripts/init_replica.js
@@ -2,9 +2,9 @@
 
 
 config = { _id: "m101", members:[
-          { _id : 0, host : "localhost:27017", priority:0, slaveDelay:5 },
-          { _id : 1, host : "localhost:27018"},
-          { _id : 2, host : "localhost:27019"} ]
+          { _id : 0, host : "yeOldeDell.local:27017", priority:0, slaveDelay:5 },
+          { _id : 1, host : "yeOldeDell.local:27018"},
+          { _id : 2, host : "yeOldeDell.local:27019"} ]
 };
 
 rs.initiate(config);
